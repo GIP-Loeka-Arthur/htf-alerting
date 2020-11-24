251184b7e473161c20c439332010fa93f47e0d6c
Arthurdw <arthur.dewitte@gmail.com>
2020-11-24 13:29:34 +0100

    Added hard coded channelHandler

--- src/fn-channelHandlerMSTeams/app.js
+++ src/fn-channelHandlerMSTeams/app.js
@@ -1,18 +1,18 @@
+const axios = require("axios");
 
-const axios = require('axios');
-
-exports.lambdaHandler = async ( event ) => {
-
-    console.log(event)
-
-    console.log("triggered channelhandler")
-
-    axios.post("https://hooks.slack.com/services/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", {
-        text: event.detail.text
-    },
-    {
-       header: {
-           "Content-Type": "application/json"
-       } 
-    }).then(() => "succesfully finished")
+exports.lambdaHandler = async (event) => {
+  for (const item of event.Records) {
+      await axios
+        .post(
+          "https://hooks.slack.com/services/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",
+          {
+            text: JSON.parse(item.body).text,
+          },
+          {
+            header: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+  }
 };
